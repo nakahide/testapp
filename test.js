420179cda026f772513534e604e80d6f2d2a8986
hide <yansuke777@gmail.com>
2019-02-10 20:09:14 +0900

    家族3追加

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var fmname = ['test1', 'test2', 'test3', 'test4', 'test5', '家族1', '家族2'];
+var fmname = ['test1', 'test2', 'test3', 'test4', 'test5', '家族1', '家族2', '家族3'];
         
         for (var i = 0; i < fmname.length; i++){
             var li = document.createElement('li');
